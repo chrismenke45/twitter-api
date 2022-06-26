42e3c5d5d797acdc91cdbea815c64fb69ec98fb5
chrismenke45 <chrismenke45@gmail.com>
2022-06-26 16:57:05 -0700

    removed app.enable(trust)

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.enable('trust proxy');
 app.set("trust proxy", 1);
 app.use(session({
   secret: process.env.SESSION_SECRET,
